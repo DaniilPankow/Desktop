f89facc09248f33822ac2217c20da1b49f3b9bde
DaniilPankow <166998025+DaniilPankow@users.noreply.github.com>
2024-09-09 15:00:42 +0300

    Add files via upload

---
+++ FourthTask.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{   static void Main()
+    {
+        Console.Write("Введіть перше число: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введіть друге число: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.Write("Введіть третє число: ");
+        int c = int.Parse(Console.ReadLine());
+
+        int max = Math.Max(Math.Max(a, b), c);
+        int min = Math.Min(Math.Min(a, b), c);
+
+        Console.WriteLine($"Максимальне значення: {max}");
+        Console.WriteLine($"Мінімальне значення: {min}");
+    }
+}
