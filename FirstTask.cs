86342b2e46287cc9f4dd0af18b2126a8fe3b4267
DaniilPankow <166998025+DaniilPankow@users.noreply.github.com>
2024-09-09 15:01:19 +0300

    Add files via upload

---
+++ FirstTask.cs
@@ -0,0 +1,25 @@
+﻿//Task 1 complete:
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть перше число (можливий день): ");
+        int number1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Введіть друге число (можливий місяць): ");
+        int number2 = int.Parse(Console.ReadLine());
+
+        bool isDay = number1 >= 1 && number1 <= 31;    
+        bool isMonth = number2 >= 1 && number2 <= 12;  
+
+        if (isDay && isMonth)
+        {
+            Console.WriteLine("Числа можуть бути днем та місяцем.");
+        }
+        else
+        {
+            Console.WriteLine("Числа не можуть бути днем та місяцем.");
+        }
+    }
+}
