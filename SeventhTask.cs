c2b66262c96d3133ea78ec8bd5471619692babad
DaniilPankow <166998025+DaniilPankow@users.noreply.github.com>
2024-09-09 14:59:42 +0300

    Add files via upload

---
+++ SeventhTask.cs
@@ -0,0 +1,14 @@
+﻿//Task 7 complete:
+using System;
+class Program
+{   
+    static void Main()
+    {
+        Console.Write("Введіть температуру в градусах Цельсія: ");
+        double celsius = double.Parse(Console.ReadLine());
+
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        Console.WriteLine($"Температура в градусах Фаренгейта: {fahrenheit}");
+    }
+}
