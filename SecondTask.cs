a6dd374760d5f013fb25773e1f8ecd2ec45d43b5
DaniilPankow <166998025+DaniilPankow@users.noreply.github.com>
2024-09-09 15:01:09 +0300

    Add files via upload

---
+++ SecondTask.cs
@@ -0,0 +1,19 @@
+﻿//Task 2 complete:
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть дійсне число: ");
+        double number = double.Parse(Console.ReadLine());
+
+        double fractionalPart = number * 100; 
+        int digits = (int)(fractionalPart % 100);
+
+        int firstDigit = digits / 10;
+        int secondDigit = digits % 10;
+
+        Console.WriteLine($"{firstDigit} + {secondDigit} = {firstDigit + secondDigit}");
+    }
+}
