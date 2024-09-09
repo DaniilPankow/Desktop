c24035acc3f857e0b885d06508f78ffd60d16309
DaniilPankow <166998025+DaniilPankow@users.noreply.github.com>
2024-09-09 15:00:30 +0300

    Add files via upload

---
+++ FifthTask.cs
@@ -0,0 +1,17 @@
+﻿//Task 5 complete:
+using System;
+class Program
+{   
+    static void Main()
+    {
+        Console.Write("Введіть довжину прямокутника: ");
+        double length = double.Parse(Console.ReadLine());
+
+        Console.Write("Введіть ширину прямокутника: ");
+        double width = double.Parse(Console.ReadLine());
+
+        double area = length * width;
+
+        Console.WriteLine($"Площа прямокутника: {area}");
+    }
+}
