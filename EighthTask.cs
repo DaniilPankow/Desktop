29d8bd243c1062e6779ef7b84ff4bed139cbcb63
DaniilPankow <166998025+DaniilPankow@users.noreply.github.com>
2024-09-09 14:58:27 +0300

    Add files via upload
    add a complete practice(probably incorrect)

---
+++ EighthTask.cs
@@ -0,0 +1,14 @@
+﻿//Task 8 complete:
+using System;
+class Program
+{   
+    static void Main()
+    {
+        Console.Write("Введіть число n (кількість натуральних чисел): ");
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = (n * (n + 1)) / 2;
+
+        Console.WriteLine($"Сума перших {n} натуральних чисел: {sum}");
+    }
+}
